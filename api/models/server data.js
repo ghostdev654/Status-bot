aee907f9af497ab9dee8a9b07bf2834058642f2c
ghostdev654 <ghostdev.js@yahoo.com>
2025-08-01 17:24:30 -0300

    Create server data.js

---
+++ api/models/server data.js
@@ -0,0 +1,7 @@
+module.exports = {
+  cpu: 0,              // Uso del CPU
+  ram: 0,              // Uso de RAM
+  subBots: 0,          // Número de sub-bots
+  status: 'Offline',   // Estado del servidor
+  lastHeartbeat: null, // Última vez que se recibieron datos
+};
